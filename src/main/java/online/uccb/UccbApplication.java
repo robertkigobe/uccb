6a9413c4a733d04ec0491f67094b7e3cfb28fe79
robertkigobe <robertkigobe@hotmail.com>
2023-01-25 20:23:04 -0500

    first commit

---
+++ src/main/java/online/uccb/UccbApplication.java
@@ -0,0 +1,13 @@
+package online.uccb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UccbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UccbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/online/uccb/UccbApplicationTests.java
@@ -0,0 +1,13 @@
+package online.uccb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UccbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
